925c9da739b6e6f604813af3718afdfdeee5b115
takuya <uv.nkmt.yii@gmail.com>
2018-05-25 21:13:16 +0900

    大まかな投稿詳細画面作成

---
+++ detail.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Takuya's site</title>
+	<link rel="stylesheet" type="text/css" href="assets/Custom.css">
+	<script type="text/javascript" src="assets/js/footerFixed.js"></script>
+	
+</head>
+<body>
+	<div class="header">
+		<p id="headTitle">Future News</p>
+		<ul id="nav-link">
+			<!-- <li class="link" id="aboutMe">About Me</li> -->
+			<li class="link" id="rec">ログアウト</li>
+		</ul>
+		<!-- <div class="link" id="aboutMe">About Me</div>
+		<div class="link" id="rec">Recommendation</div> -->
+
+
+	</div>
+	
+
+	<!-- TODO -->
+	<!-- 1.サイドバー(左)作成
+		1.ログインユーザー画像
+		2.ログインユーザー
+		3.textarea(投稿に対するコメントの内容)
+		4.コメント登録ボタン
+	2.サイドバー(右)作成：投稿に対する全ユーザーコメント欄
+		1.ユーザー名
+		2.コメント内容
+	3.投稿の詳細欄
+		1.youtubeの埋め込み動画
+		2.記事の内容 -->
+
+
+	<div class="contnets">
+
+	
+
+
+	</div>
+
+	<div id="footer">
+		<div id="copyright">Copyright (c) Takuya Nakamatsu </div>
+	</div>
+</body>
+</html>
